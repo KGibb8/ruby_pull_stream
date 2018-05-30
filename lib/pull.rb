72d0bf39f3a4951727b70f8b293324c54e25d3fd
Kieran <kieran.a.gibb@gmail.com>
2018-05-31 09:10:43 +1200

    for the moment, just check we've got a source and a sink

--- lib/pull.rb
+++ lib/pull.rb
@@ -1,17 +1,25 @@
+# frozen_string_literal: true
+
 require "pull/version"
+require "pull/source"
+require "pull/sink"
 require "pull/stream"
+require "pull/values"
+require "pull/drain"
+require "pull/collect"
 require "pull/helper"
-# require "pull/errors"
 
 module Pull
   def pull(*args)
     return Pull::Helper.new if args.empty?
 
+    # Actually we want to defer the pull_stream if a source is not provided
     source = args.first
-    raise StandardError unless source.kind_of? Pull::Source
+    raise StandardError unless source.to_s == "source"
 
+    # Actually we want to defer the pull_stream if a sink is not provided
     sink = args.last
-    raise StandardError unless sink.kind_of? Pull::Sink
+    raise StandardError unless sink.to_s == "sink"
 
     Pull::Stream.new(source, sink)
   end
