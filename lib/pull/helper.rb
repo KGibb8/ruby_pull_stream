4a46d56589db99ddee965be1ea71b1ba0ebe57fc
Kieran <kieran.a.gibb@gmail.com>
2018-05-31 09:11:16 +1200

    use pull helper to return relevant source/sinks

--- lib/pull/helper.rb
+++ lib/pull/helper.rb
@@ -1,11 +1,21 @@
+# frozen_string_literal: true
+
 module Pull
   class Helper
     def values(args)
-      Pull::Source.new(args)
+      Pull::Values.new(args)
     end
 
     def drain(callback)
-      Pull::Sink.new(callback)
+      Pull::Drain.new(callback)
+    end
+
+    def collect(callback)
+      Pull::Collect.new(callback)
+    end
+
+    def log
+      Pull::Log.new
     end
   end
 end
